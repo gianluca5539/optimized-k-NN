d49da392d8b163f8da59675b1c21db515733a95c
Gianluca Monte <gianluca.monte02@gmail.com>
2023-03-06 16:49:40 +0100

    optimized even further, a bottleneck appears to be the sorting...

--- test.py
+++ test.py
@@ -1,38 +1,29 @@
 import numpy as np
 
-def euclidean_distance(test_image, train_image):
-    # simple euclidean distance
-    return sum([(test_pixel - train_pixel) ** 2 for test_pixel, train_pixel in zip(test_image, train_image)]) ** 0.5
-
-
-def knn(test_image, images_train, labels_train, k):
-    # compute euclidean distance between test_image and each train_image
-    distances_list = [(euclidean_distance(test_image, train_image), label) 
-                      for train_image, label in zip(images_train, labels_train)]
-    print("k")
+@profile
+def knn(test_image, zipped_images_labels, k):
+    distances_list = [(np.linalg.norm(test_image - train_image), label) 
+                      for train_image, label in zipped_images_labels]
     distances_list.sort(key=lambda x: x[0])
-    # get k nearest neighbors
     k_nearest_neighbors = [label for _, label in distances_list[:k]]
-    # return the most common label
     return max(k_nearest_neighbors, key=k_nearest_neighbors.count)
 
-def test_kNN():
-    images_train = np.load('datasets/images_train.npy')[:1000]
-    labels_train = np.load('datasets/labels_train.npy')[:1000]
-    images_test = np.load('datasets/images_test.npy')[:50]
-    labels_test = np.load('datasets/labels_test.npy')[:50]
+def test_kNN(train_size,test_size,k):
+    images_train = np.load('datasets/images_train.npy')[:train_size]
+    labels_train = np.load('datasets/labels_train.npy')[:train_size]
+    images_test = np.load('datasets/images_test.npy')[:test_size]
+    labels_test = np.load('datasets/labels_test.npy')[:test_size]
+
+    zipped_images_labels = list(zip(images_train, labels_train))
 
     correct = 0
     for test_image, label in zip(images_test, labels_test):
-        prediction = knn(test_image, images_train, labels_train, 5)
+        prediction = knn(test_image, zipped_images_labels, k)
         if prediction == label:
             correct += 1
-            print("correct", prediction, label)
-        else:
-            print("incorrect", prediction, label)
-    print(f"Accuracy: {correct / len(labels_test)}")
-
+    return correct/test_size
 
 
 if __name__ == "__main__":
-    test_kNN()
+    accuracy = test_kNN(train_size=60000,test_size=100,k=3)
+    print(accuracy)
