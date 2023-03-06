8eee7d9d3089cb74a230c7b3bb260997d4269895
dario-loi <loi.1940849@studenti.uniroma1.it>
2023-03-06 19:56:04 +0100

    Precommit, type annotations, formatting

--- test.py
+++ test.py
@@ -1,18 +1,24 @@
 import numpy as np
 
-def knn(test_image, zipped_images_labels, k):
-    distances_list = np.array([(np.linalg.norm(test_image - train_image), label) 
-                      for train_image, label in zipped_images_labels])
+
+def knn(test_image: np.ndarray, zipped_images_labels, k: int) -> int:
+    distances_list = np.array(
+        [
+            (np.linalg.norm(test_image - train_image), label)
+            for train_image, label in zipped_images_labels
+        ]
+    )
     sorted_indexes = distances_list[:, 0].argsort()
     distances_list_sorted = distances_list[sorted_indexes]
     first_k_labels = distances_list_sorted[:k, 1].astype(int)
     return np.bincount(first_k_labels).argmax()
 
-def test_kNN(train_size,test_size,k):
-    images_train = np.load('datasets/images_train.npy')[:train_size]
-    labels_train = np.load('datasets/labels_train.npy')[:train_size]
-    images_test = np.load('datasets/images_test.npy')[:test_size]
-    labels_test = np.load('datasets/labels_test.npy')[:test_size]
+
+def test_kNN(train_size: int, test_size: int, k: int) -> float:
+    images_train = np.load("datasets/images_train.npy")[:train_size]
+    labels_train = np.load("datasets/labels_train.npy")[:train_size]
+    images_test = np.load("datasets/images_test.npy")[:test_size]
+    labels_test = np.load("datasets/labels_test.npy")[:test_size]
 
     zipped_images_labels = list(zip(images_train, labels_train))
 
@@ -21,9 +27,9 @@ def test_kNN(train_size,test_size,k):
         prediction = knn(test_image, zipped_images_labels, k)
         if prediction == label:
             correct += 1
-    return correct/test_size
+    return correct / test_size
 
 
 if __name__ == "__main__":
-    accuracy = test_kNN(train_size=60000,test_size=50,k=3)
+    accuracy = test_kNN(train_size=60000, test_size=50, k=3)
     print("The accuracy is: ", accuracy)
